827e5d748568079c6b59fa30bc73d363e7c6e3e2
p375894 <ahmad.ahmadov@porsche.de>
2025-06-07 15:15:47 +0200

    tutorial of upstream

--- calculator.py
+++ calculator.py
@@ -24,3 +24,13 @@ class Calculator:
     
     def matrix_multiply(self, a, b):
         return np.dot(a, b)
+    
+    #test the calculator
+    def test_calculator(self):
+        assert self.add(1, 2) == 3
+        assert self.subtract(1, 2) == -1
+        assert self.multiply(1, 2) == 2
+        assert self.divide(1, 2) == 0.5
+        assert self.square(2) == 4
+        assert self.square_root(4) == 2
+        assert self.matrix_multiply(np.array([[1, 2], [3, 4]]), np.array([[5, 6], [7, 8]])) == np.array([[19, 22], [43, 50]])
